ea002ef3460771ca92c83ba999e3e3618edf56b7
kvolodina <volodina.ksenia@gmail.com>
2023-10-17 05:00:16 +0300

    putting data to html: todo list, users select

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // Globals
+const todoList = document.getElementById('todo-list')
+const userTodo = document.getElementById('user-todo')
 let todos = []
 let users = []
 
@@ -10,8 +12,43 @@ function initApp() {
     Promise.all([getAllTodos(), getAllUsers()]).then(values => {
         [todos, users] = values
 
-        // вывести в html
+        todos.forEach(todo => printTodo(todo))
+        users.forEach(user => printUser(user))
     })
+}
+
+// Basic logic
+function getUserName(userId) {
+    const user = users.find(user => user.id === userId)
+    return user.name
+}
+
+function printTodo({id, userId, title, completed}) {
+    const li = document.createElement('li')
+    li.className = 'todo-item'
+    li.dataset.id = id
+    li.innerHTML = `<label for="${id}">${title} by <b>${getUserName(userId)}</b></label>`
+
+    const status = document.createElement('input')
+    status.type = 'checkbox'
+    status.checked = completed
+    status.id = id
+
+    const close = document.createElement('span')
+    close.className = 'close'
+    close.innerHTML = '&times;'
+
+    todoList.prepend(li)
+    li.prepend(status)
+    li.append(close)
+}
+
+function printUser({id, name}) {
+    const option = document.createElement('option')
+    option.value = id
+    option.innerText = `${name}`
+
+    userTodo.append(option)
 }
 
 // Async logic
